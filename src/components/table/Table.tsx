090d4c7b9fd1f87acb82e0bc4dba5ece8d8d951d
David Abramov <davidabramov@192.168.1.15>
2022-10-26 01:34:43 +0200

    Create static component TableItem, fix styles

--- src/components/table/Table.tsx
+++ src/components/table/Table.tsx
@@ -1,25 +1,41 @@
 import React from "react";
 import Button from "../UI/Button";
+import classes from "./Table.module.css";
+import TableItem from "./TableItem";
+
+let dummy_data = [
+  { id: "1", year: 2015, effectiveRent: 20, startingRent: 15 },
+  { id: "2", year: 2016, effectiveRent: 21, startingRent: 22 },
+  { id: "3", year: 2017, effectiveRent: 29, startingRent: 34 },
+  { id: "4", year: 2018, effectiveRent: 23, startingRent: 27 },
+  { id: "5", year: 2019, effectiveRent: 28, startingRent: 25 },
+  { id: "6", year: 2020, effectiveRent: 33, startingRent: 29 },
+  { id: "7", year: 2021, effectiveRent: 35, startingRent: 33 },
+  { id: "8", year: 2022, effectiveRent: 38, startingRent: 36 },
+];
 
 export default function Table() {
   return (
-    <div>
-      <h1>Table</h1>
-      <Button
-        title="create"
-        click={() => console.log("create")}
-        classBtn="greenBtn"
-      />
-      <Button
-        title="delete"
-        click={() => console.log("delete")}
-        classBtn="redBtn"
-      />
-      <Button
-        title="update"
-        click={() => console.log("delete")}
-        classBtn="updateBtn"
-      />
+    <div className={classes.table}>
+      <h1>Statistic</h1>
+      <section className={classes.tableHead}>
+        <div className={classes.tableHeader}>Year</div>
+        <div className={classes.tableHeader}>Starting rent</div>
+        <div className={classes.tableHeader}>Effective rent</div>
+        <div className={classes.tableHeader}>Action</div>
+      </section>
+      <section className={classes.tableContent}>
+        {dummy_data.map((item) => {
+          return (
+            <TableItem
+              year={item.year}
+              sr={item.startingRent}
+              er={item.effectiveRent}
+            />
+          );
+        })}
+      </section>
+      <Button title="add" click={() => console.log("add")} classBtn="addBtn" />
     </div>
   );
 }
--- src/components/table/TableItem.tsx
+++ src/components/table/TableItem.tsx
@@ -1,5 +1,31 @@
 import React from "react";
+import classes from "./TableItem.module.css";
+import Button from "../UI/Button";
 
-export default function TableItem() {
-  return <div>TableItem</div>;
+interface TableItemProps {
+  year: number;
+  er: number;
+  sr: number;
+}
+
+export default function TableItem({ year, sr, er }: TableItemProps) {
+  return (
+    <div className={classes.tableItem}>
+      <div>{year}</div>
+      <div>{sr}</div>
+      <div>{er}</div>
+      <div className={classes.btns}>
+        <Button
+          title="update"
+          click={() => console.log("add")}
+          classBtn="updateBtn"
+        />
+        <Button
+          title="delete"
+          click={() => console.log("add")}
+          classBtn="deleteBtn"
+        />
+      </div>
+    </div>
+  );
 }
