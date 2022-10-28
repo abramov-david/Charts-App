0e0714a923110925638ea082c09ed6267e1137c2
David Abramov <davidabramov@192.168.1.15>
2022-10-28 20:25:52 +0200

    Add mobile design to mobal modal and icon to close it

--- src/components/UI/Modal.tsx
+++ src/components/UI/Modal.tsx
@@ -21,6 +21,19 @@ export default function Modal({ children, title }: ModalProps) {
       <div className={classes.modal}>
         <h1 className={classes.title}>{title}</h1>
         {children}
+        <div className={classes.closeModalIcon} onClick={clickHandler}>
+          <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512">
+            <title>Close</title>
+            <path
+              fill="none"
+              stroke="currentColor"
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              stroke-width="32"
+              d="M368 368L144 144M368 144L144 368"
+            />
+          </svg>
+        </div>
       </div>
     </>
   );
